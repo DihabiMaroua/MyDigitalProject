bc34d74d6c7de83a289d982366752b2a215a7da7
MAROUA <dihabi.maroua1@gmail.com>
2024-04-12 13:47:00 +0200

    Remove frontend directory

--- frontend/src/pages/filtre.tsx
+++
@@ -1,7 +0,0 @@
-function Filtre () {
-    return (
-      <h1>Filtre</h1>
-    )
-  }
-  
-export default Filtre  
--- frontend/src/pages/suggestion.tsx
+++
@@ -1,7 +0,0 @@
-function Suggestion () {
-    return (
-      <h1>Suggestion</h1>
-    )
-  }
-  
-  export default Suggestion  
