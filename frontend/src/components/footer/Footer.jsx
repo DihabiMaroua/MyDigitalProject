0115012f93910103fd189d9b8f723ba2111836ab
LnTm39 <roberttom740@gmail.com>
2024-04-12 16:25:07 +0200

    Add footer and header

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -3,16 +3,30 @@ import './footer.css'
 
 function Footer () {
   return (
-    <footer>
+    <footer class="main">
+      <div className="container">
         <div class="row">
             <div class="col-md-3">
-                <p class="">Dallya</p>
-                <p><a href='/filtre' class="links-footer">Trouver ton idée cadeau</a></p>
-                <p><a href='Cagnotte'></a></p>
+              <a href='/' class="text-logo"><img src="../src/assets/logo.png" class="img-fluid" width="50px"/> Dallya</a>
+              <p class="bs-white">Inscrivez-vous à la newsletter</p>
             </div>
-            <div class="col-md-3 offset-md-1"></div>
-            <div class="col-md-4 offset-md-1"></div>
-        </div>
+            <div class="col-md-3 offset-md-1">
+              <p className='bs-white'>Dallya</p>
+              <p className='bs-white'><a>Trouver une idée cadeau</a></p>
+              <p className='bs-white'><a>Favoris</a></p>
+              <p className='bs-white'><a>Cagnotte</a></p>
+              <p className='bs-white'><a>Blog</a></p>
+              <p className='bs-white'><a>Mon compte</a></p>
+              <p className='bs-white'><a>A propos</a></p>
+            </div>
+            <div class="col-md-4 offset-md-1">
+              <p className='bs-white'>Assistance</p>
+              <p className='bs-white'><a>Politique de confidentialités</a></p>
+              <p className='bs-white'><a>Centre d'aide</a></p>
+              <p className='bs-white'><a>Mentions légales</a></p>
+              <p className='bs-white'><a>Supprimer son compte</a></p>
+            </div>
+        </div></div>
     </footer>
   )
 }
