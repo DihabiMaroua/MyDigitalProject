2c519f78865c37b18ce22bd9a1bb5f0677ac9ed5
LnTm39 <roberttom740@gmail.com>
2024-05-24 16:24:49 +0200

    Update Filtre 0.0.2

--- frontend/src/components/filtre/OccasionQuestion.jsx
+++ frontend/src/components/filtre/OccasionQuestion.jsx
@@ -31,16 +31,16 @@ const OccasionQuestion = ({ formData, handleChange, prevStep, handleSubmit }) =>
   };
 
   const settings = {
-    arrow: true,
+    dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 6,
+    slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 6,
+          slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
           dots: true
@@ -49,7 +49,7 @@ const OccasionQuestion = ({ formData, handleChange, prevStep, handleSubmit }) =>
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 1,
           slidesToScroll: 1,
           initialSlide: 2
         }
